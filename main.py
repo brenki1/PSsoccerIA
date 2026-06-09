e5230e7fb8e28579598513fd2167a572d13d4824
Osvaldo Pagioli <129009368+brenki1@users.noreply.github.com>
2026-06-08 21:38:30 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from utils import read_video, save_video
+from trackers  import Tracker
+
+def main ():
+    video_frames = read_video('input_videos/08fd33_4.mp4')
+
+    tracker = Tracker("models/best.pt")
+
+    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pk1')
+
+    save_video(video_frames, 'output_videos/video.avi')
+
+if __name__ == "__main__":
+    main()
